823ddcd4452c3e956bb8826f2eeefd2698f5026f
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2024-09-02 12:11:45 -0400

    Typography improvements

--- src/components/Forex.jsx
+++ src/components/Forex.jsx
@@ -1,7 +1,5 @@
-import { Component } from "react";
-import ReactDOM from "react-dom";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
-// import * as d3 from "d3";
 import { Layout } from "antd";
 import COLORS from "../Constants";
 import { Navbar } from "./";
@@ -15,11 +13,115 @@ import {
 
 import "../styles.css";
 
+
+export default function Forex({ name }) {
+
+  let interval = 7000;
+
+  const [coinData, setCoinData] = useState([]);
+  const [coin, setCoin] = useState('');
+  const [joke, setJoke] = useState('');
+  const [data, setData] = useState([]);
+  const [fetchDataTrigger, setFetchDataTrigger] = useState(0);
+  const fetchDataIntervalId = useRef();
+
+  const setFetchDataInterval = (interval) => {
+    // Clear old interval
+    if (fetchDataIntervalId.current) {
+      clearInterval(fetchDataIntervalId.current);
+      fetchDataIntervalId.current = undefined;
+    }
+
+    // Set new interval
+    if (interval > 0) {
+      fetchDataIntervalId.current = setInterval(() => {
+        setFetchDataTrigger(Date.now());
+      }, interval);
+    }
+  };
+
+
+  let options = {
+    method: "GET",
+    url: "https://coinranking1.p.rapidapi.com/coins",
+    params: {
+      referenceCurrencyUuid: "yhjMzLPhuIDl",
+      timePeriod: "24h",
+      tiers: "1",
+      orderBy: "marketCap",
+      orderDirection: "desc",
+      limit: "100",
+      offset: "0",
+    },
+    headers: {
+      "x-rapidapi-key": "de9f03c511msh409345b99ecf623p16aa52jsnc3bf33da52c6",
+      "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+    },
+  };
+
+
+  useEffect(() => {
+
+    try {
+      // let objDataFromAPI = [];
+      const response = axios.request(options).then((response) => {
+
+        setJoke(response.data.data.coins[0].name);
+
+        setCoinData(response.data.data.coins.map((coin) => (
+          <div className="currency-box-1">
+            {coin.name}
+            <span className="currencySymbol1">{coin.symbol}</span>
+            <div
+              style={{
+                color: coin.change > 0 ? COLORS.blue1 : COLORS.red1,
+              }}
+            >
+              {coin.change}%
+            </div>
+            ${coin.price}
+          </div>)));
+
+      }).catch((err) => console.warn(err));
+      console.log(response.data.data.coins);
+    } catch (error) {
+      console.error(error);
+    }
+
+    // Clean up for unmount to prevent memory leak
+    return () => clearInterval(fetchDataIntervalId.current);
+  }, [options, fetchDataTrigger]);
+
+
+  return <>
+    <div className="app-frame">
+      <Navbar />
+      <span className="page-heading-text">
+        {/* { this.state.coin }  */}
+
+        forex :
+        {coinData.length}
+        : {window.location.href} </span>
+      <span className="page-heading-text">{joke}</span>
+      <Layout>
+        <div className="routes"></div>
+      </Layout>
+
+      <div className="app-container">
+        {coinData}
+      </div>
+    </div>
+  </>
+}
+
+
+/*
 class Forex extends Component {
 
    //{ coinId } = useParams();
 
   state = {
+
     joke: "",
     coinData: [],
     coin:""
@@ -97,6 +199,4 @@ class Forex extends Component {
       </>
     );
   }
-}
-
-export default Forex;
+} */
