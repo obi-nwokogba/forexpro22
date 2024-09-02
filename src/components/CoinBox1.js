0fa774b37aafe2f1db8b1e96ae1a5775fc0e4133
Obi Nwokogba <83872952+obi-nwokogba@users.noreply.github.com>
2024-09-02 13:35:17 -0400

    Icons added + CSS Class improvements

--- src/components/CoinBox1.js
+++ src/components/CoinBox1.js
@@ -4,13 +4,19 @@ import "../styles.css";
 export default function CoinBox1(props) {
   return <div className='currency-box-1'>
     <div className="currency-box-inside">
-      <div className="text2"> {props.coinName} <span className="lighter">{props.coinSymbol}</span></div>
+      <div className="text3"> {props.coinName} <span className="lighter">{props.coinSymbol}</span></div>
       <div className="text4">${Number(props.coinPrice).toFixed(5)}</div>
       <div className="text7"
         style={{
           color: props.coinPriceChange > 0 ? COLORS.blue1 : COLORS.red1,
         }}
       >
+
+
+        <img
+          src={props.coinPriceChange >= 0 ? 'up-arrow.svg' : 'up-arrow.svg'}
+          className={props.coinPriceChange >= 0 ? 'up-arrow' : 'down-arrow'}
+          alt="" />
         {props.coinPriceChange}%
       </div>
     </div>
